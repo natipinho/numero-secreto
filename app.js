ed4bff799cee7ecf1e8ecfe0c74d601c7f529227
Natália Pinho <nati.pinho@gmail.com>
2026-05-26 14:51:53 +0100

    projeto inicial

---
+++ app.js
@@ -0,0 +1,23 @@
+let secretNumber = generateNumber ();
+
+//forma mais otimizada de selecionar e modificar vários elementos html
+function writeText (tag, text){
+    let field = document.querySelector (tag);
+    field.innerHTML = text;
+}
+writeText ('h1', 'Jogo do Número Secreto!');
+writeText ('p', 'Escolha um número entre 1 e 10:')
+
+
+function generateNumber (){
+   return parseInt(Math.random()* 10 + 1);
+}
+
+function verificarChute(){
+    let chosenNumber = document.querySelector('input').value; 
+    if (chosenNumber == secretNumber){
+        alert ('Parabéns! Você acertou o número secreto!');
+    } else {
+        alert ('Ops! Tente novamente!');
+    }
+}
